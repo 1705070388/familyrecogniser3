fee2320caa3f60b2110b3852d4c65daf20b7bb86
1705070388 <75253725+1705070388@users.noreply.github.com>
2021-06-22 17:39:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup() {
+    canvas = createCanvas(250, 250);
+    canvas.position(500, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/RlCPeeDIY/model.json", modelLoaded);
+}
+function modelLoaded() {
+    console.log("model loaded");
+}
+function draw() {
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("resultPersonName").innerHTML = results[0].label;
+        document.getElementById("resultPersonAccuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
